1ada44bcbf781462f705ef8b33d068e2a1eabc73
Dev singh <dev.singhh03@gmail.com>
2022-05-17 17:14:38 +0530

    Initial commit

---
+++ src/main/java/businessClasses/SignInPage.java
@@ -0,0 +1,13 @@
+package businessClasses;
+
+import com.BaseClasses.Keywords;
+
+public class SignInPage extends Keywords{
+
+		
+	public void enterEmail(String email)
+	{
+		type("Email_CSS", email);
+	}
+
+}
